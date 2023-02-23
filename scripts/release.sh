02c3df4c6d7b206b201458405ca6727f50c1c411
Matt Quinn <mattquinn@bet365.com>
2023-02-23 11:23:50 +0000

    fix: correct release build target os/arch error

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,22 +8,16 @@ function build() {
     go build -o "./release/hflow.${GOOS}.${GOARCH}" && echo "successfully built hflow.${GOOS}.${GOARCH}" || echo "error building hflow.${GOOS}.${GOARCH}"
 }
 
-ORIG_GOOS=$GOOS
-ORIG_GOARCH=$GOARCH
-
-GOOS=linux
-GOARCH=amd64
+export GOOS=linux
+export GOARCH=amd64
 build
 
-GOOS=darwin
-GOARCH=amd64
+export GOOS=darwin
+export GOARCH=amd64
 build
 
-GOOS=darwin
-GOARCH=arm64
+export GOOS=darwin
+export GOARCH=arm64
 build
-
-GOOS=$ORIG_GOOS
-GOARCH=$ORIG_GOARCH
 
 echo "release builds completed"
